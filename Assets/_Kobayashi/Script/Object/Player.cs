dff8973a57f18d763abf3861ab94458e455beff1
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2025-12-03 02:30:34 +0900

    [add]ダッシュ追加

--- Assets/_Kobayashi/Script/Object/Player.cs
+++ Assets/_Kobayashi/Script/Object/Player.cs
@@ -6,33 +6,40 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float _speed = 3f;
     [SerializeField] float _jumpPower = 5f;
+    [SerializeField] float _sprintMultiplier = 1.8f;
+    [SerializeField] float _speedLerpRate = 8f;
     [SerializeField] float _groundCheckRadius = 0.25f;
     [SerializeField] float _groundCheckDistance = 0.3f;
     [SerializeField] Transform _cameraTr;
     [SerializeField] LayerMask _groundLayer;
 
     PlayerInput _playerInput;
-    InputAction _moveAction,_jumpAction;
+    InputAction _moveAction,_jumpAction,_sprintAction;
     Rigidbody _rb;
     Quaternion _targetRot;
     Vector3 _moveInput,_move,_targetVel,_origin;
+    bool _isSprinting = false;
+    float _sprintFactor = 1f, _targetFactor;
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
         _playerInput = GetComponent<PlayerInput>();
         _moveAction = _playerInput.actions["Move"];
         _jumpAction = _playerInput.actions["Jump"];
+        _sprintAction = _playerInput.actions["Sprint"];
     }
     private void OnEnable()
     {
         _moveAction?.Enable();
         _jumpAction?.Enable();
+        _sprintAction?.Enable();
     }
 
     private void OnDisable()
     {
         _moveAction?.Disable();
         _jumpAction?.Disable();
+        _sprintAction?.Disable();
     }
     /// <summary>
     /// ÉvÉåÉCÉÑÅ[ì¸óÕ
@@ -40,6 +47,7 @@ public class Player : MonoBehaviour
     public void PlayerInput()
     {
         _moveInput = _moveAction.ReadValue<Vector2>();
+        _isSprinting = _sprintAction.IsPressed();
         if (_jumpAction.WasPressedThisFrame() && GroundChecker())
         {
             _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
@@ -53,8 +61,11 @@ public class Player : MonoBehaviour
         _move = _cameraTr.forward * _moveInput.y + _cameraTr.right * _moveInput.x;
         _move.y = 0f;
 
-        _targetVel = _move.normalized * _speed ;
-        _rb.linearVelocity = new Vector3(_targetVel.x,_rb.linearVelocity.y,_targetVel.z);
+        _targetFactor = _isSprinting ? _sprintMultiplier : 1f;
+        _sprintFactor = Mathf.Lerp(_sprintFactor, _targetFactor, Time.deltaTime * _speedLerpRate);
+
+        _targetVel = _move.normalized * (_speed * _sprintFactor);
+        _rb.linearVelocity = new Vector3(_targetVel.x, _rb.linearVelocity.y, _targetVel.z);
         if (_move != Vector3.zero)
         {
             _targetRot = Quaternion.LookRotation(_move);
