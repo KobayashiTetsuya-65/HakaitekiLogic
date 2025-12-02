cea1c3007726bff8b0549b83fb5376fc82e0ddf1
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2025-12-03 03:57:51 +0900

    [add]プレイヤーアニメーション

--- Assets/_Kobayashi/Script/Object/Player.cs
+++ Assets/_Kobayashi/Script/Object/Player.cs
@@ -1,28 +1,35 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEngine.UI.Image;
 
 public class Player : MonoBehaviour
 {
+    [Header("数値設定")]
     [SerializeField] float _speed = 3f;
     [SerializeField] float _jumpPower = 5f;
     [SerializeField] float _sprintMultiplier = 1.8f;
     [SerializeField] float _speedLerpRate = 8f;
     [SerializeField] float _groundCheckRadius = 0.25f;
     [SerializeField] float _groundCheckDistance = 0.3f;
+    [SerializeField] float _fallCheckVelocity = -2f;
+    [SerializeField] float _delayJump = 0.2f;
+
+    [Header("コンポーネント設定")]
     [SerializeField] Transform _cameraTr;
     [SerializeField] LayerMask _groundLayer;
 
+    Animator _anim;
     PlayerInput _playerInput;
     InputAction _moveAction,_jumpAction,_sprintAction;
     Rigidbody _rb;
     Quaternion _targetRot;
     Vector3 _moveInput,_move,_targetVel,_origin;
-    bool _isSprinting = false;
-    float _sprintFactor = 1f, _targetFactor;
+    bool _isSprinting = false, _isFalling = false, _isJumping = false;
+    float _sprintFactor = 1f, _targetFactor, _horizontalSpeed;
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
         _playerInput = GetComponent<PlayerInput>();
         _moveAction = _playerInput.actions["Move"];
         _jumpAction = _playerInput.actions["Jump"];
@@ -48,9 +55,10 @@ public class Player : MonoBehaviour
     {
         _moveInput = _moveAction.ReadValue<Vector2>();
         _isSprinting = _sprintAction.IsPressed();
-        if (_jumpAction.WasPressedThisFrame() && GroundChecker())
+        if (_jumpAction.WasPressedThisFrame() && GroundChecker() && !_isJumping)
         {
-            _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+            _isJumping = true;
+            StartCoroutine(Jump());
         }
     }
     /// <summary>
@@ -71,6 +79,19 @@ public class Player : MonoBehaviour
             _targetRot = Quaternion.LookRotation(_move);
             _rb.MoveRotation(Quaternion.Slerp(_rb.rotation, _targetRot, 0.15f));
         }
+        UpdateAnimator();
+    }
+    /// <summary>
+    /// アニメーター動かす
+    /// </summary>
+    private void UpdateAnimator()
+    {
+        _horizontalSpeed = new Vector3(_rb.linearVelocity.x, 0, _rb.linearVelocity.z).magnitude;
+        _isFalling = !GroundChecker() && _rb.linearVelocity.y < _fallCheckVelocity;
+
+        _anim.SetFloat("Speed", _horizontalSpeed, 0.1f,Time.deltaTime);
+        _anim.SetBool("IsGrounded", GroundChecker());
+        _anim.SetBool("IsFalling", _isFalling);
     }
     /// <summary>
     /// 接地判定
@@ -86,5 +107,18 @@ public class Player : MonoBehaviour
             _groundCheckDistance,
             _groundLayer
         );
+    }
+    /// <summary>
+    /// ジャンプ
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Jump()
+    {
+        _anim.SetTrigger("Jump");
+        yield return new WaitForSeconds(_delayJump);
+        _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+        yield return new WaitUntil(() => GroundChecker());
+        yield return new WaitForSeconds(0.05f);
+        _isJumping = false;
     }
 }
