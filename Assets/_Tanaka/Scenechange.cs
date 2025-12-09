f445743a1a04585156a716b06cfde00932556b20
陽 <youwanashityu@gmail.com>
2025-12-09 16:06:30 +0900

    [Add]シーンチェンジのスクリプトを作成

---
+++ Assets/_Tanaka/Scenechange.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenechange : MonoBehaviour
+{
+
+    [SerializeField]
+    [Header("ロードするシーン名を入力")]
+    private string sceneName = "Scene";
+
+    /// <summary>
+    /// inspector側で指定したシーン名をロードする
+    /// </summary>
+    public void ChangeScene()
+    {
+       SceneManager.LoadScene(sceneName);
+    }
+
+}
