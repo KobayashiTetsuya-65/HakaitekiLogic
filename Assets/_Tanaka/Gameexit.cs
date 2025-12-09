7edff364bc7a01b941739a26f80e1085ac12e750
陽 <youwanashityu@gmail.com>
2025-12-09 16:18:27 +0900

    [Add]ボタンにexit機能をアタッチ

--- Assets/_Tanaka/Gameexit.cs
+++ Assets/_Tanaka/Gameexit.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class Gameexit : MonoBehaviour
 {
     
+    /// <summary>
+    /// ゲームを終了する
+    /// </summary>
     public void QuitGame()
     {
         Debug.Log("ゲーム終了");
